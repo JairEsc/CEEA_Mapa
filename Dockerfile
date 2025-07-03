60aff9ddd1242fd0e1e12f4a940c20744bdf3d8b
JairEsc <p.escobar816@gmail.com>
2025-07-03 14:27:45 -0600

    multi_page

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,9 @@ FROM python:3.11-slim-buster
 
 RUN apt-get update
 RUN apt-get install nano
-
-RUN mkdir wd
-WORKDIR wd
 COPY app/requirements.txt .
 RUN pip3 install -r requirements.txt
 
 COPY app/ ./
-CMD [ "gunicorn", "--workers=1", "--threads=2", "-b", "0.0.0.0:80", "--timeout", "600", "--preload", "app:server"]
+EXPOSE 8080
+CMD [ "gunicorn", "--workers=1", "--threads=2", "-b", "0.0.0.0:8080", "--timeout", "600", "--preload", "app:server"]
--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ from funciones_auxiliares import generarMapApartirEleccion_Municipal, generarMap
 from dash.exceptions import PreventUpdate
 from flask import Flask
 
-# Carga de datos y definición de variables
+#Carga de datos y definición de variables
 shp_municipal = gpd.read_file("./assets/Datos/shp/Historicos_Acciones.shp")
 shp_regional = gpd.read_file("./assets/Datos/shp/Regional_.shp")
 shp_dosificadores = gpd.read_file("./assets/Datos/shp/Dosidicadores.shp")
@@ -25,442 +25,32 @@ map_default_municipal = funciones_auxiliares.generarMapApartirEleccion_Municipal
 map_default_regional = funciones_auxiliares.generarMapApartirEleccion_Regional(arhivo_sph=shp_regional, lista_eleccion=opciones_cloro[-1])
 map_dosificadores = funciones_auxiliares.generarMap_dosificadores(arhivo_sph = shp_dosificadores)
 
-
-#########################
-### Paleta de colores ###
-#########################
-
-# Lógica de renderizado de GeoJSON en JavaScript
-style_handle = assign("""function(feature, context){
-    const {classes, colorscale, style, colorProp} = context.hideout;
-    const value = feature.properties[colorProp];
-    for (let i = 0; i < classes.length; ++i) {
-        if (value > classes[i]) {
-            style.fillColor = colorscale[i];
-        }
-    }
-    return style;
-}""")
-
-# Clases para la paleta de colores
-classes = [-2, -0.0000000001, 0.199999999999999999, 1.5]
-colorscale = ['rgb(205,205,205)', 'rgb(255,0,0)', 'rgb(112,173,71)', 'rgb(255, 192, 0)']
-style = dict(weight=2, opacity=1, color='white', dashArray='3', fillOpacity=0.7)
-
-# Crea colorbar.
-ctg = ["{}+".format(cls, classes[i + 1]) for i, cls in enumerate(classes[:-1])] + ["{}+".format(classes[-1])]
-colorbar = dlx.categorical_colorbar(
-    categories=["No hay dato", "CI < 0.2", "0,2 <= CI <= 1.5", "CI > 1.5"],
-    colorscale=colorscale,
-    width=300,
-    height=30,
-    position="bottomleft",
-    className="colorbar_custom"
-)
-
-
-#########################################
-### Definimos parámetros interactivos ###
-#########################################
-
-# Creación de GeoJSON.
-geojson = dl.GeoJSON(
-    data=map_default_municipal,
-    style=style_handle,
-    zoomToBounds=False,
-    zoomToBoundsOnClick=True,
-    hoverStyle=arrow_function(dict(weight=5, color='#666', dashArray='')),
-    hideout=dict(colorscale=colorscale, classes=classes, style=style, colorProp="Valor-actual"),
-    id="geojson"
-)
-
-geojson_dosificadores = dl.GeoJSON(
-    data=map_dosificadores
-)
-############################################
-### Definición de Componentes del Layout ###
-############################################
-
-encabezado = dbc.Row([
-    dbc.Col(
-        html.H2("Indicadores de Calidad del Agua ", style={'color': 'white', 'margin': '0', 'padding': '2vh 0 0 10px'}),
-        width=7, xxl=7, xl=7, lg=7, md=7, sm=12, xs=12,
-        style={'backgroundColor': 'rgb(157, 36, 73)', 'padding': '0', 'margin': '0'}
-    ),
-    dbc.Col(
-        html.A(
-            html.Img(src="./assets/Imagenes/Planeacion_dorado.png", style={'width': '100%', 'height': '70%', 'padding': '1vh 0 0 10px'}),
-            href="http://sigeh.hidalgo.gob.mx/",
-            target="_blank"
-        ),
-        width=3, xxl=3, xl=3, lg=3, md=3, sm=7, xs=7,
-        style={'backgroundColor': 'rgb(157, 36, 73)', 'padding': '0', 'margin': '0'}
-    ),
-    dbc.Col(
-        html.A(
-            html.Img(src="./assets/Imagenes/CEAA_dorado.png", style={'width': '75%', 'height': '75%', 'padding': '1vh 0 0 10px'}),
-            href="https://ceaa.hidalgo.gob.mx/",
-            target="_blank"
-        ),
-        width=2, xxl=2, xl=2, lg=2, md=2, sm=5, xs=5,
-        style={'backgroundColor': 'rgb(157, 36, 73)', 'padding': '0', 'margin': '0'}
-    )
-],
-    style={"height": "12vh", 'width': '100vw', 'padding': '0', 'margin': '0'}
-)
-
-
-
-
-########################
-### Botone Laterales ###
-########################
-
-layers_icon = html.I(id="layers_icon", className="bi bi-layers", style={'margin': '0', 'paddin': '0'})
-search_icon = html.I(id="search_icon", className="bi bi-search", style={'margin': '0', 'paddin': '0'})
-information_icon = html.I(id="about_information_icon", className="bi bi-book",  style={'margin': '0', 'paddin': '0'})
-question_icon = html.I(id="question_icon", className="bi bi-question-lg",  style={'margin': '0', 'paddin': '0'})
-
-
-botton_layers = dbc.Button(
-    [layers_icon],
-    id="botton_layers_icon",
-    color="primary",
-    n_clicks=0,
-    size="sm",
-    outline=True,
-    className="button-custom",
-    style={'width': '70%', 'height': '6vh', 'margin': '1vh 10% 1vh 10%'}
-)
-
-botton_search = dbc.Button(
-    [search_icon],
-    id="botton_search_icon",
-    color="primary",
-    n_clicks=0,
-    size="sm",
-    outline=True,
-    className="button-custom",
-    style={'width': '70%', 'height': '6vh', 'margin': '1vh 10% 1vh 10%'}
-)
-
-botton_information = dbc.Button(
-    [information_icon],
-    id="botton_information_icon",
-    color="primary",
-    n_clicks=0,
-    size="sm",
-    outline=True,
-    className="button-custom",
-    style={'width': '70%', 'height': '6vh', 'margin': '1vh 10% 1vh 10%'}
-)
-
-botton_question = dbc.Button(
-    [question_icon],
-    id="botton_question_icon",
-    color="primary",
-    n_clicks=0,
-    size="sm",
-    outline=True,
-    className="button-custom",
-    style={'width': '70%', 'height': '6vh', 'margin': '1vh 10% 1vh 10%'}
-)
-
-
-###########################
-### Offcanvas and Modals###
-###########################
-
-########################
-### OffCanvas Layers ###
-########################
-
-botton_municipal = dbc.Button(
-    "Municipal",
-    id= "botton_municipal",
-    color="primary",
-    n_clicks=0,
-    size="sm",
-    outline=True,
-    className="button-custom active",
-    style={'width': '70%', 'height': '6vh', 'margin': '1vh 10% 1vh 10%'}
-)
-
-botton_regional = dbc.Button(
-    "Regional",
-    id= "botton_regional",
-    color="primary",
-    n_clicks=0,
-    size="sm",
-    outline=True,
-    className="button-custom",
-    style={'width': '70%', 'height': '6vh', 'margin': '1vh 10% 1vh 10%'}
-)
-
-slider_periodo = dcc.Slider(
-    id="slider_periodo",
-    step=None,
-    marks=anios,
-    value=list(anios.keys())[-1],
-    className="slider-custom"
-)
-
-play_pause_icon = html.I(id="play_pause", className="bi bi-play-fill")
-
-botton_time = dbc.Button(
-    ["Histórico", play_pause_icon],
-    id="botton_time",
-    color="primary",
-    n_clicks=0,
-    size="sm",
-    outline=True,
-    className="button-custom",
-    style={'width': '70%', 'height': '5vh', 'margin': '1vh 10% 1vh 10%'}
-)
-
-intervalo_tiempo = dcc.Interval(
-    id="intervalo_tiempo",
-    interval=2500,  # 1000 = 1s
-    n_intervals=2024,  # Valor inicial
-    disabled=True
-)
-
-
-offcanvas_layers = html.Div(
-    [
-        dbc.Offcanvas(
-            children=[ 
-                html.H5("Tipo de mapa", style={'color': 'black'}),
-                html.Div( 
-                    children= [ botton_municipal, botton_regional],
-                    style={"display": "flex", "justifyContent": "space-around"}
-            ),
-                html.Br(),
-                html.H5("Periodo", style={'color': 'black'}),
-                slider_periodo,
-                html.Br(),
-                html.Br(),
-                html.H5("Explora el tiempo", style={'color': 'black'}),
-                botton_time,
-                intervalo_tiempo
-            ],
-            id="offcanvas_layers",
-            title= html.H4("Capas de información", style={'textAlign': 'center', 'color': 'black'}),
-            is_open=False,
-            backdrop=False,
-            style={"height": "88vh", "marginTop": "12vh", "backgroundColor": " #c1c0c0"}
-        ),
-    ],
-)
-
-
-
-########################
-### OffCanvas Search ###
-########################
-
 municipal_geo = funciones_auxiliares.obtenerCentroides_Municipales(shp_municipal)
 print(len(municipal_geo.latitud.unique()))
 regional_geo = funciones_auxiliares.obtenerCentroides_Regionales(shp_regional)
 
-# Dropdown para buscar municipios o regiones según el mapa actual.
-buscador = dcc.Dropdown(
-    id='buscador',
-    options=[{'label': mun, 'value': latitud} for mun, latitud in zip(municipal_geo.NOM_MUN, municipal_geo.latitud)],
-    placeholder="Buscar:",
-    clearable=False,
-    className="buscador_custom"
-)
 
-offcanvas_search = html.Div(
-    [
-        dbc.Offcanvas( 
-            children = [
-                buscador
-            ],
-            id="offcanvas_search",
-            title= html.H4("Busca tu municipio o región", style={'textAlign': 'center', 'color': 'black'}),
-            is_open=False,
-            backdrop=False,
-            style={"height": "88vh", "marginTop": "12vh", "backgroundColor": " #c1c0c0"},
-        ),
-    ]
-)
-
-
-
-
-modal_information = dbc.Modal(children=[
-    dbc.ModalHeader(dbc.ModalTitle("Información Adicional")),
-    dbc.ModalBody([
-        "La Norma Oficial Mexicana ",
-        html.A("NOM-127-SSA1-2021", href="https://www.dof.gob.mx/nota_detalle_popup.php?codigo=5650705", target="_blank"),
-        " establece que el agua de uso y consumo humano debe presentar una concentración de cloro residual libre entre 0.2 y 1.5mg/L."
-    ]),
-    dbc.ModalFooter(
-        dbc.Button("De Acuerdo", id="close_information", className="ms-auto", n_clicks=0)
-    ),
-],
-    id="modal_information",
-    is_open=False,
-)
-
-modal_content = [
-    dbc.ModalHeader(
-        dbc.ModalTitle("Explora el mapa")
-    ),
-    dbc.ModalBody(
-        html.Div([
-            html.P(
-                "Este mapa web interactivo tiene una barra lateral en la parte izquierda con cuatro secciones principales:"
-            ),
-            html.Ol([
-                html.Li([
-                    html.Strong("Capas de Información:"),
-                    html.Ul([
-                        html.Li("Permite elegir el tipo de mapa a visualizar (municipal o regional)."),
-                        html.Li("Incluye una línea de tiempo para seleccionar el año deseado."),
-                        html.Li('Tiene un botón "Histórico" que cambia el mapa automáticamente cada 2.5 segundos para mostrar diferentes periodos.')
-                    ])
-                ]),
-                html.Li([
-                    html.Strong("Buscador:"),
-                    html.Ul([
-                        html.Li("Facilita buscar un municipio o una región, según el mapa que estés viendo."),
-                        html.Li("Al buscar, el mapa se centra en el área seleccionada.")
-                    ])
-                ]),
-                html.Li([
-                    html.Strong("Información Adicional:"),
-                    html.P("Ofrece detalles y explicaciones más específicas sobre los indicadores mostrados.")
-                ]),
-                html.Li([
-                    html.Strong("Explora el Mapa:"),
-                    html.P("Es la opción que has seleccionado para interactuar directamente con el mapa.")
-                ])
-            ]),
-            html.P("Cada sección está diseñada para que puedas navegar y obtener la información que necesites de manera sencilla y visual.")
-        ])
-    )
-]
-
-modal_question = dbc.Modal(
-    children=[
-        *modal_content,
-        dbc.ModalFooter(
-            dbc.Button("De Acuerdo", id="close_question", className="ms-auto", n_clicks=0)
-        ),
-    ],
-    id="modal_question",
-    is_open=False,
-)
-
-##################################
-### Barra vertical interactiva ###
-##################################
-
-vertical_nav = dbc.Nav(
-    [
-        dbc.NavLink(children = botton_layers, id="layers_nav", style = {'margin': '0', 'padding': '0'}),
-        dbc.NavLink(children = botton_search, id="search_nav", style = {'margin': '0', 'padding': '0'}),
-        dbc.NavLink(children = botton_information, id="information_nav", style = {'margin': '0', 'padding': '0'}),
-        dbc.NavLink(children = botton_question, id="question_nav", style = {'margin': '0', 'padding': '0'}),
-    ],
-    vertical=True,
-    pills=True,
-    style={
-        "height": "100vh",
-        'width': '6vw',
-        "padding": "0",
-        'margin': '0',
-        'backgroundColor': 'rgb(179, 142, 93)'
-    }
-)
-
-
-###############
-### Lateral ###
-###############
-
-barra_lateral = html.Div(
-    children= vertical_nav,
-    id="barra_lateral",
-    className="barra_lateral",
-    style={
-        "position": "absolute",
-        "backgroundColor": "black",
-        "height": "100vh", 
-        'width': '6vw',
-        "zIndex": "1000"
-    },
-)
-
-# Mapa
-mapa = dbc.Row(
-    children=[
-        dbc.Col(
-            dl.Map(
-                id="mapa",  # Id asignado para usar en callbacks
-                children=[
-                    dl.TileLayer(),
-                    dl.LayersControl(
-                        children=[
-                            dl.BaseLayer(children=[geojson], name="Cloro Residual Libre", checked=True),
-                            dl.Overlay(children=[geojson_dosificadores], name="Dosificadores de Cloro", checked=False),
-                        ],
-                        position="topright",
-                        id="layers_control",
-                        collapsed= False,  # Para que el control de capas esté expandido por defecto
-                    ),
-                    barra_lateral,
-                    dl.ZoomControl(position="topleft"),
-                ],
-                center=[20.41509, -98.82936],  # Coordenadas iniciales
-                zoom=9,
-                viewport={"center": [20.41509, -98.82936], "zoom": 9},  # Cambiado a viewport
-                zoomControl=False,
-                style={'height': '88vh'}
-            ),
-            width=12, xxl=12, xl=12, lg=12, md=12, sm=12, xs=12,
-            style={'backgroundColor': '#000000', 'padding': '0', 'margin': '0'}
-        )
-    ],
-    style={"height": "88vh", 'width': '100vw', 'padding': '0', 'margin': '0'}
-)
-
-
-
-##############
-### Layout ###
-##############
-
-simbologia_imagen = html.Div(
-    html.Img(src="assets/Imagenes/simbologia_corel.png", style={'width': 'auto', 'height': '24vh', 'margin': '0 0 0 0'}),
-    style={
-        'position': 'absolute',
-        'bottom': '25px',
-        'right': '10px',
-        'zIndex': '1000'
-    },
-    className="simbologia_imagen_custom"
-)
 server = Flask(__name__)
 
-app = dash.Dash(server=server, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME, dbc.icons.BOOTSTRAP,"assets/Style.css"],)
+app = dash.Dash(__name__,server, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME, dbc.icons.BOOTSTRAP,"assets/Style.css"],use_pages=True)
+app.layout = html.Div([
+    dash.page_container,
 
-app.layout = dbc.Container([
-    encabezado,
-    mapa,
-    offcanvas_layers,
-    offcanvas_search,
-    modal_information,
-    modal_question,
-    simbologia_imagen,
-    dcc.Store(id="current_map", data="municipal"),  # Almacena el estado actual del mapa
-],
-    fluid=True,
-    style={'height': '100vh', 'width': '100vw', 'padding': '0', 'margin': '0'}
-)
+])
+# app.layout = dbc.Container([
+#     dash.page_container,
+#     encabezado,
+#     mapa,
+#     offcanvas_layers,
+#     offcanvas_search,
+#     modal_information,
+#     modal_question,
+#     simbologia_imagen,
+#     dcc.Store(id="current_map", data="municipal"),  # Almacena el estado actual del mapa
+# ],
+#     fluid=True,
+#     style={'height': '100vh', 'width': '100vw', 'padding': '0', 'margin': '0'}
+# )
 
 
 #####################################
@@ -640,6 +230,23 @@ def update_map(latitud, current_map):
     # Actualiza el viewport del mapa con la nueva ubicación
     return {"center": [latitud, longitud], "zoom": 12}
 
+
+@app.callback(
+    dash.dependencies.Output('url', 'href'),
+    [dash.dependencies.Input('navigate-button', 'n_clicks')]
+)
+def navigate(n_clicks):
+    if n_clicks:
+        return '/not_home'
+    return dash.no_update
+@app.callback(
+    dash.dependencies.Output('url2', 'href'),
+    [dash.dependencies.Input('navigate-button2', 'n_clicks')]
+)
+def navigate2(n_clicks):
+    if n_clicks:
+        return '/'
+    return dash.no_update
 #################
 ### Copilador ###
 #################
